faf7ae6b838cf486f84c9545887e2a523d93bfd3
KIMPPT <dudghks9303@gmail.com>
2023-05-18 12:35:30 +0900

    05181235

--- react-css/src/App.js
+++ react-css/src/App.js
@@ -2,12 +2,22 @@ import "./App.css";
 import CounterComp from "./features/counter/CounterComp";
 import InputForm from "./features/inputform/InputForm";
 import StyleComp from "./features/styleComponent/StyleComp";
+import StyleButton from "./features/styleComponent/StyleButton";
 function App() {
   return (
     <div className="App">
       <CounterComp />
-      <InputForm/>
-      <StyleComp/>
+      <InputForm />
+      <StyleComp />
+      <StyleButton color="red" bgcolor="pink" fontsize="3rem">
+        버튼
+      </StyleButton>
+      <StyleButton color="red" bgcolor="pink" fontsize="2rem">
+        버튼
+      </StyleButton>
+      <StyleButton color="red" bgcolor="pink" fontsize="1rem">
+        버튼
+      </StyleButton>
     </div>
   );
 }
---
+++ react-css/src/features/styleComponent/StyleButton.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { styled } from "styled-components";
+/*styled-components 를 이용해 버튼 만들기*/
+let Button = styled.a`
+  color: ${(props) => props.color};
+  background-color: ${(props) => props.bgcolor};
+  /*font-size 크기를 받아와서 그 크기대로 출력 */
+  font-size: ${(props) => props.fontsize};
+`;
+//...rest는 나머지 연산자로 남은 매개변수의 값들을 배열/객체로 들고온다
+export default function StyleButton({ children, ...rest }) {
+  console.log(children);
+  //console.log에서 볼 때는 ...을 빼고 쳐야 한다
+  console.log(rest);
+  //위의 props는 rest가 되어서 props.color는 rest.color 즉, rest 객체 중 color의 값을 들고온다
+  return (
+    <div>
+      {/*아래 ...은 스프레드 연산자로 안의 값을 풀어서 씀 */}
+      <Button {...rest}>{children}</Button>
+    </div>
+  );
+}
